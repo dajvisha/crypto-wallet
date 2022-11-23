bd48043ad126e49a86ed5d2ee134fe1fb4110e6a
dajvisha <diegojosevilla@outlook.com>
2022-11-23 07:50:49 -0800

    component: improve stucture for card component

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,6 +1,60 @@
 import styles from './styles.module.css';
 
-function Card(props) {
+function Header(props) {
+    const { style, className, children } = props;
+    const classNames = `
+        ${className}
+        ${styles['card-header']}
+    `;
+
+    return (
+        <div style={style} className={classNames}>
+            {children}
+        </div>
+    );
+}
+
+function Title(props) {
+    const { title } = props;
+
+    if (!title) {
+        return;
+    }
+
+    return (
+        <h4 className={styles['card-title']}>{title}</h4>
+    );
+}
+
+function Body(props) {
+    const { className, children } = props;
+    const classNames = `
+        ${className}
+        ${styles['card-body']}
+    `;
+
+    return (
+        <div
+            className={classNames}>
+            {children}
+        </div>
+    );
+}
+
+function Row(props) {
+    const { children } = props;
+
+    return (
+        <div>{children}</div>
+    );
+}
+
+Card.Header = Header;
+Card.Title = Title;
+Card.Body = Body;
+Card.Row = Row;
+
+export function Card(props) {
     const {
         shadow,
         style,
@@ -9,15 +63,15 @@ function Card(props) {
         children,
     } = props;
 
-    const classNames = `${className} ${styles.card} ${shadow && styles.shadow}`;
+    const classNames = `
+        ${className}
+        ${styles.card}
+        ${shadow && styles['card-shadow']}
+    `;
+
     return (
-        <div
-            style={style}
-            className={classNames}>
-            {title && <h4 className={styles.title}>{title}</h4>}
-            { children }
+        <div style={style} className={classNames}>
+            {children}
         </div>
     );
 }
-
-export default Card;
