4e94546f32b6d9cf7a9116519f6e15f9b7dc4f87
dajvisha <diegojosevilla@outlook.com>
2022-11-22 21:28:01 -0800

    component: add style prop to Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,7 @@ import styles from './styles.module.css';
 function Card(props) {
     const {
         shadow,
+        style,
         className,
         children,
     } = props;
@@ -10,6 +11,7 @@ function Card(props) {
     const classNames = `${className} ${styles.card} ${shadow && styles['card-shadow']}`;
     return (
         <div
+            style={style}
             className={classNames}>
             { children }
         </div>
