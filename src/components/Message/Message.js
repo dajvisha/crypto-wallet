97a65c4ae5e301f82f028adab4b070262059da06
dajvisha <diegojosevilla@outlook.com>
2022-11-22 18:10:12 -0800

    component: add message component

---
+++ src/components/Message/Message.js
@@ -0,0 +1,18 @@
+import styles from './styles.module.css';
+
+function Message(props) {
+    const {
+        type,
+        message,
+    } = props;
+
+    const classNames = `${styles.message} ${styles[type]}`;
+
+    return (
+        <div className={classNames}>
+            {message}
+        </div>
+    );
+}
+
+export default Message;
